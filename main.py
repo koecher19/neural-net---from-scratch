c2166a8fda1b6e0d5a4954229c9a19f6a1227542
Felix <75081082+koecher19@users.noreply.github.com>
2022-11-22 20:46:44 +0100

    training with mnist data set

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+'''
+Neural Network to train on minst data set
+using 'Neuronale Netze selbst programmieren', O'reilly, 2017
+'''
+
 import numpy as np
 import scipy.special
+import matplotlib.pyplot as plt
 
 
 class NeuralNetwork:
@@ -60,6 +66,32 @@ class NeuralNetwork:
 
 if __name__ == '__main__':
     print('starting programm ...')
-    n = NeuralNetwork(3, 3, 3, 0.3)
 
+    input_nodes = 784
+    hidden_nodes = 100
+    output_nodes = 10
+    learning_rate = 0.3
 
+    n = NeuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+
+    # load mnist data set
+    training_data_file = open('mnist_train.csv', 'r')
+    training_data_list = training_data_file.readlines()
+    training_data_file.close()
+
+    # go through all the records in training data set
+    print('training ...')
+    for record in training_data_list:
+        all_values = training_data_list[0].split(',')
+        image_array = np.asfarray(all_values[1:]).reshape((28, 28))
+        # display current picture:
+        '''plt.imshow(image_array, cmap='Greys', interpolation='None')
+        plt.show()'''
+        scaled_input = (np.asfarray(all_values[1:]) / 255 * 0.99) + 0.01
+        targets = np.zeros(output_nodes) + 0.01
+
+        # all_values[0] is the target label for this record
+        targets[int(all_values[0])] = 0.99
+        n.train(scaled_input, targets)
+        pass
+    print('finished training')
